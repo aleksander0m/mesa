43c8e034d5514669563d0501fa6ff40e6519e657
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2017-04-10 15:54:22 +0200

    Android: Build DRM platform

--- src/egl/drivers/dri2/egl_dri2.h
+++ src/egl/drivers/dri2/egl_dri2.h
@@ -283,7 +283,7 @@ struct dri2_egl_surface
    struct gbm_dri_surface *gbm_surf;
 #endif
 
-#if defined(HAVE_WAYLAND_PLATFORM) || defined(HAVE_DRM_PLATFORM)
+#if defined(HAVE_WAYLAND_PLATFORM) || defined(HAVE_ANDROID_PLATFORM) || defined(HAVE_DRM_PLATFORM)
    __DRIbuffer           *dri_buffers[__DRI_BUFFER_COUNT];
    struct {
 #ifdef HAVE_WAYLAND_PLATFORM
@@ -298,6 +298,9 @@ struct dri2_egl_surface
 #ifdef HAVE_DRM_PLATFORM
       struct gbm_bo       *bo;
 #endif
+#ifdef HAVE_ANDROID_PLATFORM
+      struct ANativeWindowBuffer *buffer;
+#endif
       bool                locked;
       int                 age;
    } color_buffers[4], *back, *current;
@@ -311,15 +314,6 @@ struct dri2_egl_surface
 
    /* EGL-owned buffers */
    __DRIbuffer           *local_buffers[__DRI_BUFFER_COUNT];
-
-   /* Used to record all the buffers created by ANativeWindow and their ages.
-    * Usually Android uses at most triple buffers in ANativeWindow
-    * so hardcode the number of color_buffers to 3.
-    */
-   struct {
-      struct ANativeWindowBuffer *buffer;
-      int age;
-   } color_buffers[3], *back;
 #endif
 
 #if defined(HAVE_SURFACELESS_PLATFORM)
